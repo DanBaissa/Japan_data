2f5131c29a1731990727af63b7d3abb87c8c288d
danbaissa <danbaissa@gmail.com>
2020-10-21 19:34:42 -0400

    almost done

--- Japan_code.R
+++ Japan_code.R
@@ -9,7 +9,7 @@ library(readxl)
 library(gtools)
 library(votingpower)
 
-Japan2<-read_excel("C:\\Users\\jgrei\\Dropbox\\Dissertation materials\\JapanClean2.xlsx")
+Japan2<-read_excel("JapanClean2.xlsx")
 
 Japan2$LDPCombined<-0
 
@@ -19,11 +19,14 @@ summary(Japan2$LDPCombined)
 sum(Japan2$ku_voteshare[which(Japan2$LDPCombined==1)])
 
 
-#Vote Share Loops
-Japan2$LDPVS83<-0
+dist<- unique(Japan2$kucode)
 
-Japan2$LDPVS83<-if()
-  
-  
-  
+year <- unique(Japan2$year)
+
+Japan2$Test <- 0
+
+Japan2$Test[which(Japan2$kucode == dist[1] & Japan2$year == year[1])] <- sum(Japan2$ku_voteshare[which(Japan2$LDPCombined==1 & Japan2$year == year[1] & Japan2$kucode == dist[1])])
+
+# All that needs to be done now is turn the code in line 28 into a for loop. dist[1] would be replaced with dist[i] and year [1] replaced with year[j].
+
   
